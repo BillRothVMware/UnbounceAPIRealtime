6142c9028a92a6bd41dd15de910a6df78ecd0765
Bill Roth <broth@vmware.com>
2018-10-19 12:51:51 -0700

    Update unbounce-data.sh

--- unbounce-data.sh
+++ unbounce-data.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 # by bill roth
 
-curl -u "<YourAPIKey>:" -H "Accept: application/vnd.unbounce.api.v0.4+json" -X GET https://api.unbounce.com/pages\?with_stats=true\&limit=100 | \
+curl -u "<YourAPIKey>:" -H "Accept: application/vnd.unbounce.api.v0.4+json" -X GET https://api.unbounce.com/pages\?with_stats=true\&limit=999 | \
 jq -c -r '.pages[]| "\(.url) \(.id) \(.state) \(.tests.current.visits) \(.tests.current.visitors) \(.tests.current.clicks) \(.tests.current.conversions)"' |\
 sed 's/https\:\/\///g' |\
 awk -f /home/broth/code/unbounce/pjq.awk  |\
